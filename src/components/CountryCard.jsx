16c6d5890cdce2af0fea72acdde91596e9cb6be4
g-akca <gok.akca66@gmail.com>
2026-03-23 12:14:23 +0300

    Fetch countries from data.json

--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -1,15 +1,15 @@
-function CountryCard() {
+function CountryCard({ flagUrl, name, population, region, capital }) {
   return (
     <div className="bg-white rounded-[5px] overflow-hidden shadow-[0_0_7px_rgba(0,0,0,0.1)]">
-      <img src="https://flagcdn.com/w320/tr.png" className="w-full h-40 object-cover" />
+      <img src={flagUrl} className="w-full h-40 object-cover" />
 
       <div className="px-6 pt-6 pb-12 flex flex-col gap-4">
-        <p className="text-[18px] font-extrabold leading-base">Turkey</p>
+        <p className="text-[18px] font-extrabold leading-base">{name}</p>
 
         <ul className="font-light leading-4 flex flex-col gap-2">
-          <li><span className="font-semibold">Population:</span> 84,339,067</li>
-          <li><span className="font-semibold">Region:</span> Asia</li>
-          <li><span className="font-semibold">Capital:</span> Ankara</li>
+          <li><span className="font-semibold">Population:</span> {population}</li>
+          <li><span className="font-semibold">Region:</span> {region}</li>
+          <li><span className="font-semibold">Capital:</span> {capital}</li>
         </ul>
       </div>
     </div>
--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -1,12 +1,19 @@
 import CountryCard from "./CountryCard";
 
+import data from "../data/data.json";
+
 function CountryList() {
   return (
     <section className="grid grid-cols-[repeat(auto-fit,264px)] gap-10 justify-center tablet:gap-18">
-      <CountryCard />
-      <CountryCard />
-      <CountryCard />
-      <CountryCard />
+      {data.map(item => (
+        <CountryCard
+          flagUrl={item.flag}
+          name={item.name}
+          population={item.population}
+          region={item.region}
+          capital={item.capital}
+        />
+      ))}
     </section>
   );
 }
