d16795bc67cd480192ad6a6244ce8dce5f311a80
g-akca <gok.akca66@gmail.com>
2026-03-23 16:32:37 +0300

    Change some styles

--- src/components/CountryInfo.jsx
+++ src/components/CountryInfo.jsx
@@ -6,8 +6,8 @@ function CountryInfo({ name }) {
   const countryData = data.find(item => item.name === name);
 
   return (
-    <section className="flex flex-col gap-12 tablet:gap-14 desktop:flex-row desktop:gap-30 desktop:items-center">
-      <img src={countryData.flag} alt={`${name} flag`} className="self-stretch rounded-md tablet:rounded-[10px] desktop:self-center desktop:max-w-140" />
+    <section className="flex flex-col gap-12 tablet:gap-14 desktop:grid desktop:grid-cols-[1fr_1.2fr] desktop:gap-30 desktop:items-center">
+      <img src={countryData.flag} alt={`${name} flag`} className="w-full rounded-md tablet:rounded-[10px]" />
 
       <div className="flex flex-col gap-4 tablet:gap-6 desktop:grow">
         <p className="text-[24px] leading-[137.5%] font-extrabold tablet:text-[32px]">{countryData.name}</p>
