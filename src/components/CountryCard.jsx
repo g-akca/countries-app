272788d6824b795f1360bf7a9b9e6b2f36148b3e
g-akca <gok.akca66@gmail.com>
2026-03-23 10:37:39 +0300

    Give CountryList styles, create CountryCard

---
+++ src/components/CountryCard.jsx
@@ -0,0 +1,9 @@
+function CountryCard() {
+  return (
+    <div>
+      a
+    </div>
+  )
+}
+
+export default CountryCard;
--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -1,8 +1,14 @@
+import CountryCard from "./CountryCard";
+
 function CountryList() {
   return (
-    <>
-    </>
-  )
+    <section className="grid grid-cols-[repeat(auto-fill,264px)] gap-10 justify-center">
+      <CountryCard />
+      <CountryCard />
+      <CountryCard />
+      <CountryCard />
+    </section>
+  );
 }
 
 export default CountryList;
