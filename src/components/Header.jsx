6c9d5332f766a82446c898689187b68b093d30bd
g-akca <gok.akca66@gmail.com>
2026-03-23 07:49:00 +0300

    Set up FontAwesome, add ThemeToggler styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import ThemeToggler from "./ThemeToggler";
 
 function Header() {
   return (
-    <header className="bg-white h-20 px-4 flex justify-between items-center shadow-[0_2px_4px_rgba(0,0,0,0.05)]">
+    <header className="bg-white h-20 px-4 flex justify-between items-center shadow-[0_2px_7px_rgba(0,0,0,0.07)]">
       <p className="font-extrabold leading-5">Where in the world?</p>
 
       <ThemeToggler />
--- src/components/ThemeToggler.jsx
+++ src/components/ThemeToggler.jsx
@@ -1,7 +1,12 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMoon } from "@fortawesome/free-regular-svg-icons";
+
 function ThemeToggler() {
   return (
-    <>
-    </>
+    <button type="button" className="flex items-center gap-1.5 cursor-pointer">
+      <FontAwesomeIcon icon={faMoon} className="text-[16px]" />
+      <p className="text-[12px] font-semibold leading-[135%]">Dark Mode</p>
+    </button>
   )
 }
 
