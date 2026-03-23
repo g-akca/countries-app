3d754f4b97b12b364a98029d6fdf8689144d3f25
g-akca <gok.akca66@gmail.com>
2026-03-23 19:59:43 +0300

    Add dark mode styles for details page

--- src/components/BackButton.jsx
+++ src/components/BackButton.jsx
@@ -4,7 +4,7 @@ import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
 function BackButton() {
   return (
-    <Link to="/" className="w-26 h-8 flex justify-center items-center gap-2 cursor-pointer bg-white shadow-[0_0_7px_rgba(0,0,0,0.1)] rounded-xs tablet:w-34 tablet:h-10">
+    <Link to="/" className="w-26 h-8 flex justify-center items-center gap-2 cursor-pointer bg-white dark:bg-blue-900 shadow-[0_0_7px_rgba(0,0,0,0.1)] rounded-xs tablet:w-34 tablet:h-10">
       <FontAwesomeIcon icon={faArrowLeft} className="text-[16px] tablet:text-[18px]" />
 
       <span className="font-light pt-0.5 tablet:text-[16px]">Back</span>
--- src/components/CountryButton.jsx
+++ src/components/CountryButton.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function CountryButton({ name }) {
   return (
-    <Link to={`/country/${name}`} className="min-h-7 w-24 bg-white shadow-[0_0_4px_rgba(0,0,0,0.1)] flex justify-center items-center text-center">
+    <Link to={`/country/${name}`} className="min-h-7 w-24 p-2 bg-white dark:bg-blue-900 shadow-[0_0_4px_rgba(0,0,0,0.1)] flex justify-center items-center text-center">
       {name}
     </Link>
   )
