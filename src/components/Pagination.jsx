4bf0264e6f0293a153db077b9d28f9ad05bc5387
g-akca <gok.akca66@gmail.com>
2026-03-23 19:53:26 +0300

    Add dark mode styles for Pagination

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -2,7 +2,11 @@ import MuiPagination from "@mui/material/Pagination";
 import Stack from "@mui/material/Stack";
 import useMediaQuery from "@mui/material/useMediaQuery";
 
+import { useTheme } from "../context/ThemeProvider";
+
 function Pagination({ currentPage, setCurrentPage, totalPages }) {
+  const { darkMode } = useTheme();
+
   const isDesktop = useMediaQuery("(min-width: 1440px)");
   const isTablet = useMediaQuery("(min-width: 768px)");
 
@@ -20,8 +24,27 @@ function Pagination({ currentPage, setCurrentPage, totalPages }) {
         size={isDesktop ? "large" : isTablet ? "medium" : "small"}
         sx={{
           "& .MuiPaginationItem-root": {
-            fontFamily: "'Nunito Sans', sans-serif",
+            fontFamily: "'Nunito Sans', sans-serif"
           },
+          ...(darkMode && {
+            "& .MuiPaginationItem-root": {
+              color: "white"
+            },
+            "& .MuiPaginationItem-root.Mui-selected": {
+              backgroundColor: "white",
+              color: "black"
+            },
+            "& .MuiPaginationItem-root.Mui-disabled": {
+              color: "#777"
+            },
+            "& .MuiPaginationItem-root:hover": {
+              backgroundColor: "rgba(255, 255, 255, 0.1)",
+              color: "white"
+            },
+            "& .MuiSvgIcon-root": {
+              color: "white"
+            }
+          })
         }}
       />
     </Stack>
