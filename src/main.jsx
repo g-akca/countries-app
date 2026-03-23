2fab11cad09fb3947ce2343c7d7f69d3b4bedf17
g-akca <gok.akca66@gmail.com>
2026-03-23 13:04:55 +0300

    Create NavLink to go to homepage

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/countries-app">
+    <BrowserRouter basename="/countries-app/">
       <App />
     </BrowserRouter>
   </StrictMode>,
