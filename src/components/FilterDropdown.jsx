7479b30d83654baf29442143bd16e76a0d70d108
g-akca <gok.akca66@gmail.com>
2026-03-23 09:49:56 +0300

    Create custom dropdown

--- src/components/FilterDropdown.jsx
+++ src/components/FilterDropdown.jsx
@@ -1,7 +1,32 @@
+import { useState } from "react";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+
 function FilterDropdown() {
+  const [open, setOpen] = useState(false);
+  const [region, setRegion] = useState("");
+
+  const options = ["Africa", "America", "Asia", "Europe", "Oceania"];
+
   return (
-    <>
-    </>
+    <div className="relative px-6 h-12 w-50 bg-white shadow-[0_2px_9px_rgba(0,0,0,0.06)] flex items-center rounded-[5px] text-[12px] leading-[135%]">
+      <button type="button" onClick={() => setOpen(prev => !prev)} className="cursor-pointer grow flex justify-between items-center">
+        <p>{region ? region : "Filter by Region"}</p>
+
+        <FontAwesomeIcon icon={faChevronDown} className="text-[8px]" />
+      </button>
+
+      {open && (
+        <ul className="absolute left-0 top-full mt-2 px-6 py-4 bg-white rounded-[5px] w-50 shadow-[0_2px_9px_rgba(0,0,0,0.06)] flex flex-col gap-2">
+          {options.map(item => (
+            <li>
+              <button onClick={() => setRegion(item)} className="w-full text-start">{item}</button>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
   )
 }
 
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,7 +3,7 @@ import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 function SearchBar() {
   return (
-    <div className="bg-white px-8 h-12 flex gap-5 items-center shadow-[0_2px_9px_rgba(0,0,0,0.06)]">
+    <div className="bg-white px-8 h-12 flex gap-5 items-center shadow-[0_2px_9px_rgba(0,0,0,0.06)] rounded-[5px]">
       <FontAwesomeIcon icon={faMagnifyingGlass} className="text-[16px] text-grey-300" />
 
       <input 
